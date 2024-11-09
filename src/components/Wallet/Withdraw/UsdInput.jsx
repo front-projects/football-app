cb4bd308b8c77f5c960814827e4d9a44f5d46d71
Front-projects <sasnykvlad2@gmail.com>
2024-11-09 16:55:39 +0200

    inputs upd

--- src/components/Wallet/Withdraw/UsdInput.jsx
+++ src/components/Wallet/Withdraw/UsdInput.jsx
@@ -2,9 +2,45 @@ const UsdInput = () => {
   return (
     <div className="w-full flex flex-col">
       <div className="bg-[#FFFFFF66] w-full rounded-[28px] text-[20px] flex">
-        <input type="text" className="w-full rounded-[28px]" />
-        <div className="">Test</div>
+        <input
+          type="text"
+          className="rounded-[28px] py-[22px] pl-[20px]"
+          placeholder="Enter an amount"
+        />
+        <div className="bg-white rounded-[28px] text-[#37C100] flex items-center justify-center w-full">
+          MAX
+        </div>
       </div>
+      <p className="text-[14px] pl-[30px] py-2">Your Banas 100,000 USD</p>
+      <div className="w-full grid grid-cols-3 gap-2 text-[20px]">
+        <div className="border-[3px] border-white rounded-[28px] py-[16px] text-center">
+          25%
+        </div>
+        <div className="border-[3px] border-white rounded-[28px] py-[16px] text-center">
+          50%
+        </div>
+        <div className="border-[3px] border-white rounded-[28px] py-[16px] text-center">
+          75%
+        </div>
+      </div>
+      <div className="bg-[#FFFFFF66] w-full rounded-[28px] text-[20px] flex mt-6">
+        <input
+          type="text"
+          className="rounded-[28px] py-[22px] pl-[20px]"
+          placeholder="Enter an amount"
+          value={100000}
+        />
+        <div className="bg-white rounded-[28px] text-[#37C100] flex items-center justify-center w-full">
+          USDT
+        </div>
+      </div>
+      <button className="text-[#37C100] bg-[#E7FF2B] text-[30px] rounded-[28px] py-[27px] mt-[20px]">
+        Withdraw{" "}
+      </button>
+      <p className="text-[#FFFFFF] text-[11px] text-center px-[30px] mt-6">
+        Make sure that the data is entered correctly, in case of an error the
+        funds may be lost forever
+      </p>
     </div>
   );
 };
--- src/util/back/requests.js
+++ src/util/back/requests.js
@@ -3,201 +3,210 @@
 // const URL = "https://blockcreater.com:6054/"; // TEST
 // const URL = "https://footballearn.site:6044/"; //PROD
 
-export const getAllPlayers = async () => {
-  return [
-    {
-      name: "Player One",
-      price: 1000,
-      value: 15.5,
-      photo: "./images/person/1.png",
-    },
-    {
-      name: "Player Two",
-      price: 2000,
-      value: 20,
-      photo: "./images/person/2.png",
-    },
-    {
-      name: "Player Three",
-      price: 1500,
-      value: 18.7,
-      photo: "./images/person/3.png",
-    },
-    {
-      name: "Player Four",
-      price: 1000,
-      value: 15.5,
-      photo: "./images/person/4.png",
-    },
-    {
-      name: "Player Five",
-      price: 2000,
-      value: 20,
-      photo: "./images/person/5.png",
-    },
-    {
-      name: "Player Fifth",
-      price: 1500,
-      value: 18.7,
-      photo: "./images/person/6.png",
-    },
-  ];
-};
+const TYPE = "DEV";
 
+export const getAllPlayers = async () => {
+  if (TYPE == "DEV") {
+    return [
+      {
+        name: "Player One",
+        price: 1000,
+        value: 15.5,
+        photo: "./images/person/1.png",
+      },
+      {
+        name: "Player Two",
+        price: 2000,
+        value: 20,
+        photo: "./images/person/2.png",
+      },
+      {
+        name: "Player Three",
+        price: 1500,
+        value: 18.7,
+        photo: "./images/person/3.png",
+      },
+      {
+        name: "Player Four",
+        price: 1000,
+        value: 15.5,
+        photo: "./images/person/4.png",
+      },
+      {
+        name: "Player Five",
+        price: 2000,
+        value: 20,
+        photo: "./images/person/5.png",
+      },
+      {
+        name: "Player Fifth",
+        price: 1500,
+        value: 18.7,
+        photo: "./images/person/6.png",
+      },
+    ];
+  }
+};
 export const getAllBalls = async () => {
-  return [
-    {
-      name: "Test",
-      price: 100,
-      photo: "./images/ball/1.png",
-    },
-    {
-      name: "Test",
-      price: 100,
-      photo: "./images/ball/2.png",
-    },
-    {
-      name: "Test",
-      price: 100,
-      photo: "./images/ball/3.png",
-    },
-    {
-      name: "Test",
-      price: 100,
-      photo: "./images/ball/4.png",
-    },
-    {
-      name: "Test",
-      price: 100,
-      photo: "./images/ball/5.png",
-    },
-    {
-      name: "Test",
-      price: 100,
-      photo: "./images/ball/6.png",
-    },
-  ];
+  if (TYPE == "DEV") {
+    return [
+      {
+        name: "Test",
+        price: 100,
+        photo: "./images/ball/1.png",
+      },
+      {
+        name: "Test",
+        price: 100,
+        photo: "./images/ball/2.png",
+      },
+      {
+        name: "Test",
+        price: 100,
+        photo: "./images/ball/3.png",
+      },
+      {
+        name: "Test",
+        price: 100,
+        photo: "./images/ball/4.png",
+      },
+      {
+        name: "Test",
+        price: 100,
+        photo: "./images/ball/5.png",
+      },
+      {
+        name: "Test",
+        price: 100,
+        photo: "./images/ball/6.png",
+      },
+    ];
+  }
 };
 
 export const getListUsers = async () => {
-  return {
-    balance: 0,
-    poolUserDtoList: [
-      {
-        amount: 0,
-        nameSurname: "1",
-        photo: "string",
-      },
-      {
-        amount: 100,
-        nameSurname: "2",
-        photo: "string",
-      },
-      {
-        amount: 0,
-        nameSurname: "3",
-        photo: "string",
-      },
-      {
-        amount: 0,
-        nameSurname: "4",
-        photo: "string",
-      },
-      {
-        amount: 0,
-        nameSurname: "5",
-        photo: "string",
-      },
-      {
-        amount: 0,
-        nameSurname: "6",
-        photo: "string",
-      },
-      {
-        amount: 100,
-        nameSurname: "7",
-        photo: "string",
-      },
-      {
-        amount: 0,
-        nameSurname: "8",
-        photo: "string",
-      },
-      {
-        amount: 0,
-        nameSurname: "9",
-        photo: "string",
-      },
-      {
-        amount: 0,
-        nameSurname: "10",
-        photo: "string",
-      },
-    ],
-  };
+  if (TYPE == "DEV") {
+    return {
+      balance: 0,
+      poolUserDtoList: [
+        {
+          amount: 0,
+          nameSurname: "1",
+          photo: "string",
+        },
+        {
+          amount: 100,
+          nameSurname: "2",
+          photo: "string",
+        },
+        {
+          amount: 0,
+          nameSurname: "3",
+          photo: "string",
+        },
+        {
+          amount: 0,
+          nameSurname: "4",
+          photo: "string",
+        },
+        {
+          amount: 0,
+          nameSurname: "5",
+          photo: "string",
+        },
+        {
+          amount: 0,
+          nameSurname: "6",
+          photo: "string",
+        },
+        {
+          amount: 100,
+          nameSurname: "7",
+          photo: "string",
+        },
+        {
+          amount: 0,
+          nameSurname: "8",
+          photo: "string",
+        },
+        {
+          amount: 0,
+          nameSurname: "9",
+          photo: "string",
+        },
+        {
+          amount: 0,
+          nameSurname: "10",
+          photo: "string",
+        },
+      ],
+    };
+  }
 };
 
 export const getHistory = async () => {
-  return [
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-    {
-      name: "Test name",
-      type: "minus",
-      price: 14,
-    },
-  ];
+  if (TYPE == "DEV") {
+    return [
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+      {
+        name: "Test name",
+        type: "minus",
+        price: 14,
+      },
+    ];
+  }
 };
