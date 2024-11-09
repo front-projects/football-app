7408a321b164b2bd9056d9aa5dc61b88800d64fe
Front-projects <sasnykvlad2@gmail.com>
2024-11-09 23:52:17 +0200

    upd

--- src/components/Home/HomeName.jsx
+++ src/components/Home/HomeName.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { EditIcon } from "../UI/icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { updateUserName } from "../../util/back/requests";
+import ErrorAlert from "../UI/errorAlert";
+import { setUser } from "../../store/auth-slice";
 
 export default function HomeName() {
   const user = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
   const [name, setName] = useState(user.username);
   const [isEditing, setIsEditing] = useState();
   const [isLoading, setIsLoading] = useState();
@@ -15,54 +18,62 @@ export default function HomeName() {
     setIsLoading(true);
     const response = await updateUserName(user.telegramId, name);
     if (response) {
+      dispatch(setUser({ ...user, username: name }));
       setIsEditing(false);
       setIsLoading(false);
     } else {
       setIsError(true);
+      setTimeout(() => {
+        setIsError(false);
+      }, 2500);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div
-      className="flex items-end gap-[10px] mt-[25px]"
-      onClick={() => setIsEditing(true)}
-    >
-      {isEditing ? (
-        <input
-          type="text"
-          value={name}
-          autoFocus
-          className="max-w-[100px]"
-          onChange={(e) => setName(e.target.value)}
-        />
-      ) : (
-        <p className="text-white/40 text-[14px]">{name}</p>
-      )}
-
-      <div className="h-max ">
+    <>
+      {isError && <ErrorAlert />}
+      <div
+        className="flex items-end gap-[10px] mt-[25px]"
+        onClick={() => setIsEditing(true)}
+      >
         {isEditing ? (
-          <div className="flex gap-2">
-            <div
-              className="bg-[#E7FF2B] rounded-[28px] text-[#37C100] px-2"
-              onClick={submitEditing}
-            >
-              {isLoading ? "Saving..." : "Save"}
-            </div>
-            <div
-              className="border-[#37C100] border-2 rounded-[28px] text-white px-2"
-              onClick={(e) => {
-                e.stopPropagation();
-                setIsEditing(false);
-                setName(user.username);
-              }}
-            >
-              Cancel
-            </div>
-          </div>
+          <input
+            type="text"
+            value={name}
+            autoFocus
+            className="max-w-[100px]"
+            onChange={(e) => setName(e.target.value)}
+          />
         ) : (
-          <EditIcon />
+          <p className="text-white/40 text-[14px]">{name}</p>
         )}
+
+        <div className="h-max ">
+          {isEditing ? (
+            <div className="flex gap-2">
+              <div
+                className="bg-[#E7FF2B] rounded-[28px] text-[#37C100] px-2"
+                onClick={submitEditing}
+              >
+                {isLoading ? "Saving..." : "Save"}
+              </div>
+              <div
+                className="border-[#37C100] border-2 rounded-[28px] text-white px-2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsEditing(false);
+                  setName(user.username);
+                }}
+              >
+                Cancel
+              </div>
+            </div>
+          ) : (
+            <EditIcon />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/UI/errorAlert.jsx
@@ -0,0 +1,26 @@
+import gsap from "gsap";
+import { useEffect } from "react";
+
+const ErrorAlert = () => {
+  useEffect(() => {
+    const anim = gsap.to("#error-alert", { translateY: 0 });
+    const timeout = setTimeout(() => {
+      gsap.to("#error-alert", { translateY: "-100px" });
+    }, 2000);
+    return () => {
+      anim.kill();
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <div
+      id="error-alert"
+      className="fixed inset-0 w-screen bg-gray-600/80 h-max py-4 text-center text-white rounded-b-[20px] transform -translate-y-[100px]"
+    >
+      Something went wrong. Try again
+    </div>
+  );
+};
+
+export default ErrorAlert;
