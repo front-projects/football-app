07191299e3fe179fc9d6794fc2e8ef5b41cc6968
Front-projects <sasnykvlad2@gmail.com>
2024-11-11 20:36:16 +0200

    upd

--- src/components/Bonus/PromoBonus.jsx
+++ src/components/Bonus/PromoBonus.jsx
@@ -6,8 +6,8 @@ const PromoBonus = () => {
           +3 USD
         </h1>
         <p className="text-[11px] text-[#E7FF2B]">Promo code activation</p>
-        <div className="bg-[#FFFFFF66] w-full rounded-[28px] mt-[11px] py-[10px]  text-[11px]">
-          - - - -
+        <div className="bg-[#FFFFFF66] w-full rounded-[28px] mt-[11px] py-[10px] text-[11px] flex items-center justify-center">
+          <input className="w-[40px]" maxLength={4} placeholder="- - - -" />
         </div>
         <div className="absolute left-1/2 -translate-x-1/2 -bottom-4 py-2 bg-[#E7FF2B] text-[#37C100] rounded-[28px] w-max px-[20px] text-[11px]">
           Get a bonus
