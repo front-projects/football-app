fdd812819f892e7cc0a3c3e25094c1cf0077338e
Front-projects <sasnykvlad2@gmail.com>
2024-11-11 20:14:49 +0200

    upd

--- src/components/Bonus/InviteButton.jsx
+++ src/components/Bonus/InviteButton.jsx
@@ -1,9 +1,20 @@
 import { InviteIcon } from "../UI/icons";
+import WebApp from "@twa-dev/sdk";
 
 const InviteButton = () => {
+  const inviteFriend = () => {
+    WebApp.HapticFeedback.impactOccurred("light");
+    const text = `Text for invite`;
+    const url = `https://t.me/share/url?url=&text=${encodeURIComponent(text)}`;
+    WebApp.openTelegramLink(url);
+  };
+
   return (
     <>
-      <button className="text-[#37C100] bg-[#E7FF2B] mt-[40px] max-xsmall:mt-[30px] rounded-[28px] py-3 w-full flex items-center justify-center gap-[20px]">
+      <button
+        onClick={inviteFriend}
+        className="text-[#37C100] bg-[#E7FF2B] mt-[40px] max-xsmall:mt-[30px] rounded-[28px] py-3 w-full flex items-center justify-center gap-[20px]"
+      >
         <p className="text-[18px] pt-1 max-xsmall:text-[14px]">
           Invite a friend
         </p>
