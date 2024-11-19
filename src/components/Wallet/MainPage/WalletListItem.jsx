43226fc907d0804bd6753927c774ac771459779b
Front-projects <sasnykvlad2@gmail.com>
2024-11-19 23:36:30 +0200

    upd

--- src/components/Wallet/MainPage/WalletListItem.jsx
+++ src/components/Wallet/MainPage/WalletListItem.jsx
@@ -1,6 +1,7 @@
 import { WalletDollarIcon, WalletItemBg } from "../../UI/icons";
 
 const WalletListItem = ({ data }) => {
+  console.log(data);
   return (
     <div className="h-[63px] w-full relative wallet-item opacity-0 translate-y-[20px]">
       <div className="absolute inset-0">
@@ -8,7 +9,10 @@ const WalletListItem = ({ data }) => {
       </div>
       <div className="h-full w-full inset-0 absolute py-[12px] px-[38px] flex justify-between items-center">
         <div className="h-[39px] w-[39px]">
-          <img src="./images/wallet-item.png" alt="" />
+          <img
+            src={`sftp://root@88.222.220.73/root/${data.photo}`}
+            alt="Test"
+          />
         </div>
         <div className="flex flex-col items-center max-w-[70%]">
           <p className="text-[18px]">{data.nameSurname}</p>
