71a38aaea1e052106b7ac81c3cd041c1e6cad7cb
front-projects <sasnykvlad2@gmail.com>
2024-12-19 01:34:28 +0200

    api url

--- src/util/back/requests.js
+++ src/util/back/requests.js
@@ -4,9 +4,9 @@ import WebApp from "@twa-dev/sdk";
 import axios from "axios";
 
 // const URL = "https://blockcreater.com:6054/"; // TEST
-const URL = "https://footballearn.site:6044/api/v1/"; //PROD
-// const TYPE = "DEV";
-const TYPE = "PROD";
+const URL = "https://fotball-learn.site:6044/api/v1/"; //PROD
+const TYPE = "DEV";
+// const TYPE = "PROD";
 
 // export const TG_ID = "kleinheisterkamp";
 export const TG_ID = WebApp.initDataUnsafe.user.username;
